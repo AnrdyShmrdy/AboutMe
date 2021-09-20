628ed127f895959c9c1815a526e4025f2f8465f8
AnrdyShmrdy <andy.ponce@outlook.com>
2021-09-19 21:36:21 -0400

    Added toast messages upon clicking start activity buttons Added toast message upon clicking back button once the approve button was clicked Project now fulfills the requirements for the assignment

--- app/src/main/java/edu/floridapoly/mobiledeviceapps/fall21/andrewponce_aboutme/FavoriteActivity.java
+++ app/src/main/java/edu/floridapoly/mobiledeviceapps/fall21/andrewponce_aboutme/FavoriteActivity.java
@@ -4,17 +4,21 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class FavoriteActivity extends AppCompatActivity {
-
+    String[] favorite = {"favName", "favType", "favDesc"};
+    Boolean isApproved = false;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_favorite);
 
         Intent intent = getIntent();
-        String[] favorite = intent.getStringArrayExtra(MainActivity.Name);
+        favorite = intent.getStringArrayExtra(MainActivity.Name);
         String favName = favorite[0];
         String favType = favorite[1];
         String favDesc = favorite[2];
@@ -24,5 +28,25 @@ public class FavoriteActivity extends AppCompatActivity {
         TextView favNameTxtView = findViewById(R.id.favName_txtViewVal);
         favNameTxtView.setText(favName);
         favDescTxtView.setText(favDesc);
+
+    }
+    public void approveBtnPress(View view){
+        Button approve_btn = findViewById(R.id.approve_btn);
+        approve_btn.setText("approved");
+        isApproved = true;
+    }
+    @Override
+    public void onBackPressed()
+    {
+        super.onBackPressed();
+        if(isApproved) {
+
+            Button approve_btn = findViewById(R.id.approve_btn);
+            String type = favorite[1];
+            Toast.makeText(getApplicationContext(),
+                    "Returning from " + type + ", Received: APPROVED!",
+                    Toast.LENGTH_SHORT)
+                    .show();
+        }
     }
 }
--- app/src/main/java/edu/floridapoly/mobiledeviceapps/fall21/andrewponce_aboutme/MainActivity.java
+++ app/src/main/java/edu/floridapoly/mobiledeviceapps/fall21/andrewponce_aboutme/MainActivity.java
@@ -6,6 +6,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
@@ -15,30 +16,39 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
-    public void sendFavCourse(View view){
+    private void toastMyName(){
+        TextView myName = findViewById(R.id.myName_txtViewVal);
         Toast.makeText(getApplicationContext(),
-                "Course Activity Button Pressed!",
+                "name: " + myName.getText(),
                 Toast.LENGTH_SHORT)
                 .show();
+    }
+    private void toastFavInfo(String type, String name){ //toast message with fav name and type
+        Toast.makeText(getApplicationContext(),
+                type + ": " + name,
+                Toast.LENGTH_SHORT)
+                .show();
+    }
+    public void sendFavCourse(View view){
         Intent intent = new Intent(this, FavoriteActivity.class);
         EditText editText = findViewById(R.id.favCourse_editTxt);
         String favName = editText.getText().toString();
         String favType = "course";
         String favDesc = "Favorite Course";
+        toastMyName();
+        toastFavInfo(favType,favName);
         String [] favorite = {favName, favType, favDesc};
         intent.putExtra(Name,favorite);
         startActivity(intent);
     }
     public void sendFavSong(View view){
-        Toast.makeText(getApplicationContext(),
-                "Song Activity Button Pressed!",
-                Toast.LENGTH_SHORT)
-                .show();
         Intent intent = new Intent(this, FavoriteActivity.class);
         EditText editText = findViewById(R.id.favSong_editTxt);
         String favName = editText.getText().toString();
         String favType = "song";
         String favDesc = "Favorite Song";
+        toastMyName();
+        toastFavInfo(favType,favName);
         String [] favorite = {favName, favType, favDesc};
         intent.putExtra(Name,favorite);
         startActivity(intent);
